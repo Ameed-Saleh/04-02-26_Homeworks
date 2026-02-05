6a3a03e3b7fc8951b1b6295ebb175c019c652c24
Ameed-Saleh <ameed.saleh@outlook.com>
2026-02-05 13:23:22 +0200

    str - function

--- 04-02-26_Five_Tasks/Q4-post_office_delivery.py
+++ 04-02-26_Five_Tasks/Q4-post_office_delivery.py
@@ -2,13 +2,13 @@
 last_name = str("PERETZ")
 first_name= str("danny")
 country = str("IL")
-city_address = str(" Tel Aviv")
-zipcode = "2012800"
+city_address = str("Tel Aviv")
+zipcode = int("2012800")
 
-
+print("")
 print("details of post office delivery  ")
 print("")
 print(f"FOR : {last_name} {first_name}")
 print(f"COUNTRY : {country}")
 print(f"ADDRESS : {city_address}")
-print(f"ZIPCODE :{zipcode}")
+print(f"ZIPCODE : {zipcode}")
